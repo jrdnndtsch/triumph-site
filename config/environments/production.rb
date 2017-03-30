161e5182c64fad674e3e9d3b98575f00e8316b3e
trudy macnabb <trudy.macnabb@gmail.com>
2017-03-30 14:19:08 -0400

    asset precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'donovanbailey.herokuapp.com',
+    :domain         => 'triumphant.herokuapp.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
